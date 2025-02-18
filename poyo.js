ecj2/poyo

commit af86da886117a706dc403df7defec33e3ca5b6dd

Store color channel properties as integers
When calling Poyo.createColor(), store the returned object's color
channel properties as integers ranging from 0 to 255 instead of as
decimals ranging from 0.0 to 1.0.

I find it easier to work in this range when manipulating the properties
of a color variable than to do so in decimal.

poyo.js
@@ -1000,7 +1000,7 @@ let Poyo = new class {
 
   createColor(r, g, b, a = 255) {
 
-    return {r: r / 255, g: g / 255, b: b / 255, a: a / 255};
+    return {r: r, g: g, b: b, a: a};
   }
 
   loadFont(path, style = this.STYLE_NORMAL) {
@@ -1201,10 +1201,10 @@ let Poyo = new class {
       this.font.canvas.height = this.target.height;
     }
 
-    let r = color.r * 255;
-    let g = color.g * 255;
-    let b = color.b * 255;
-    let a = color.a;
+    let r = color.r;
+    let g = color.g;
+    let b = color.b;
+    let a = color.a / 255;
 
     // Set font properties.
     this.font.context.textAlign = alignment;
@@ -1689,7 +1689,7 @@ let Poyo = new class {
 
       offsets[0], offsets[1], offsets[2], offsets[3],
 
-      tint.r, tint.g, tint.b, tint.a
+      tint.r / 255, tint.g / 255, tint.b / 255, tint.a / 255
     );
   }
 
@@ -1701,7 +1701,7 @@ let Poyo = new class {
     if (c[0] != t.r || c[1] != t.g || c[2] != t.b || c[3] != t.a)  {
 
       // Upload the tint.
-      this.WebGL2.uniform4fv(this.uniforms.u_tint, [tint.r, tint.g, tint.b, tint.a]);
+      this.WebGL2.uniform4fv(this.uniforms.u_tint, [tint.r / 255, tint.g / 255, tint.b / 255, tint.a / 255]);
 
       // Cache the tint for next time.
       this.cache.tint = [t.r, t.g, t.b, t.a];

commit 40f2be5c1b16f8f264848ee53d83d3488f5cd2c4

Fix clearToColor()
Divide color channels by 255 to convert them to decimals, thus fixing
what commit af86da8 broke.

poyo.js
@@ -857,7 +857,7 @@ let Poyo = new class {
 
   clearToColor(color) {
 
-    this.WebGL2.clearColor(color.r, color.g, color.b, color.a);
+    this.WebGL2.clearColor(color.r / 255, color.g / 255, color.b / 255, color.a / 255);
 
     this.WebGL2.clear(this.WebGL2.COLOR_BUFFER_BIT);
   }